e47212cbe8145a2947c67576c0003dfd54183237
Richard Crosby <richard.crosby@protonmail.com>
2021-05-29 11:18:32 +0200

    fix(app): remove video-react and font css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
-import '../node_modules/video-react/dist/video-react.css';
-import '../public/fonts/Inter/Inter.css';
 import theme from '../styles/theme';
 
 const App = ({ Component, pageProps }) => {
