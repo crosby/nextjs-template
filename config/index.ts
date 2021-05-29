1843d491a4ac8052a59ade7519467fa450557997
Richard Crosby <richard.crosby@protonmail.com>
2021-05-29 12:21:23 +0200

    feat: google tag manager added

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  gtm: {
+    id: process.env.NEXT_PUBLIC_GTM_ID,
+  },
+};
+
+export default config;
---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import config from 'config';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${config.gtm.id}');`,
+            }}
+          />
+        </Head>
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${config.gtm.id}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
